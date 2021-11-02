a39d210e144a6aeb4785ac8dc72c3ac19d597995
manaminahata <manami.nahata@rakus-partners.co.jp>
2021-11-02 12:07:59 +0900

    出力画面の作成

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -26,28 +27,36 @@ public class Exam04Controller {
 		return "exam04";
 	}
 	
-	// 入力画面の出力
 	// 今回の場合未入力項目があればindexメソッドに戻るように設定する
 	@RequestMapping("/result")
-	public String result(UserForm userForm,
+	public String result(@Validated UserForm userForm,
 			BindingResult result,
 			RedirectAttributes redirectAtrributes,
 			Model model) {
+		
+		// 未入力項目があった時の処理（if文は上から処理されるため、一番上に記載すること）
+				if (result.hasErrors()) {
+					return index();
+				}
+		
 		// ドメインクラスをインスタンス化
 		User user = new User();
-		
+				
 		// userFormオブジェクトからuserオブジェクトへプロパティをコピー
 		BeanUtils.copyProperties(userForm, user);
 		
-		// flashスコープにuserの格納
-		redirectAtrributes.addAttribute("user", user);
+		// プロパティが異なるため、手動で値をコピー
+		user.setAge(userForm.getIntAge());
 		
-		// 未入力項目があった時の処理
-		if (result.hasErrors()) {
-			return "index()";
-		}
+		// flashスコープにuserの格納（userのなかにageも含まれる）
+		redirectAtrributes.addFlashAttribute("user", user);
 		
-		// 問題なく処理できたら下記をリターン
+		// 問題なく処理できたら次のメソッドへ
+		return "redirect:/exam04/toresult";
+	}
+	
+	@RequestMapping("toresult")
+	public String toresult() {
 		return "exam04-result";
 	}
 }
